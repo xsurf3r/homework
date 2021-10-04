0f0993c545aeec3c7dad4757fed5f707332eb651
xsurf3r <veingxd@gmail.com>
2021-10-04 13:52:56 +0300

    modified:   if.py

---
+++ if.py
@@ -0,0 +1,36 @@
+'''
+Renats Ornicans
+04.10.2021
+'''
+
+a=609
+
+if a==609:
+
+    print (1)
+
+elif a:
+
+    print(2)
+
+else:
+
+    print(3)
+
+b=111
+
+input(b)
+
+print(b)
+
+b=input()
+
+c=input()
+
+if b==c:
+
+    print(1)
+
+elif b == (18):
+
+    print(2)
