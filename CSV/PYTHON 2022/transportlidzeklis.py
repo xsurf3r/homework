e562f19480f1341467838d986a41479a39eda62b
xsurf3r <veingxd@gmail.com>
2022-09-12 12:01:31 +0300

    seveneleven

---
+++ CSV/PYTHON 2022/transportlidzeklis.py
@@ -0,0 +1,15 @@
+class transportlidzekils():
+    def __init__(self,modelis,krasa,motoraTilpums,atrumkarba):
+        self.modelis = modelis
+        self.krasa = krasa
+        self.motoraTilpums =motoraTilpums
+        self.atrumkarba = atrumkarba
+
+    def dati(self):
+        print(f"modelis: {self.modelis}")
+        print(f"krasa: {self.krasa}")
+        print(f"motora tilpums: {self.motoraTilpums}")
+        print(f"atrumkarba: {self.atrumkarba}")
+
+auto = transportlidzekils("otlander","melna","10","manuāla")
+auto.dati()
